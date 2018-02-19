0cbd00ed9c66d9c02503046b5cc7c699ca85142f
Marco Segreto <msecret@users.noreply.github.com>
2018-02-18 22:49:37 -0800

    Make the footer a color

--- client/src/View/Footer.elm
+++ client/src/View/Footer.elm
@@ -1,8 +1,11 @@
 module View.Footer exposing (..)
 
+import Css exposing (..)
 import Html
 import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css, src)
 import View.Logo as Logo
+import Style.Theme exposing (..)
 
 
 type alias Model =
@@ -11,5 +14,5 @@ type alias Model =
 
 view : Html msg
 view =
-    footer []
+    footer [ css [ backgroundColor colors.secondary ] ]
         [ Logo.view ]

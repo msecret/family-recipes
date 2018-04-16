889bb74d5113bb2d0180500e1dd43b850fc6f102
Marco Segreto <msecret@users.noreply.github.com>
2018-04-16 14:20:52 -0700

    Try at graphql  query

--- client/src/Types/Cook.elm
+++ client/src/Types/Cook.elm
@@ -1,5 +1,8 @@
 module Types.Caok exposing (..)
 
+import GraphQL.Request.Builder exposing (..)
+import GraphQL.Request.Builder.Arg as Arg
+import GraphQL.Request.Builder.Variable as Var
 import Types.Image exposing (..)
 
 
@@ -15,3 +18,27 @@ type alias Cook =
     , updatedAt : Number
     , createdAt : Number
     }
+
+
+cookQuery : Document Query Cook { vars | cookId : String }
+cookQuery =
+    let
+        cookIdVar =
+            Var.required "cookId" .cookId Var.id
+
+        cook =
+            object Cook
+                |> with (field "email" [] string)
+                |> with (field "location" [] string)
+                |> with (field "firstName" [] string)
+                |> with (field "lastName" [] string)
+                |> with (field "fullName" [] string)
+
+        queryRoot =
+            extract
+                (field "cook"
+                    [ ( "id", Arg.variable cookIdVar ) ]
+                    cook
+                )
+    in
+        queryDocument queryRoot

0bc1a0ffa5978dfebecdfd173b8448e445839ea4
Marco Segreto <msecret@users.noreply.github.com>
2018-06-16 23:33:15 -0700

    Added more seeding

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -10,119 +10,269 @@ require 'faker'
 
 Image.destroy_all
 
-Image.create!(
+ianita = Image.create!(
   name: 'Anita Ferroggiaro',
   url: 'anita.jpg',
   alt: 'Picture of Anita Ferroggiaro',
+  width: 960,
+  height: 960,
   width_orig: 960,
   height_orig: 960,
 )
 
-Image.create!(
+ipaul = Image.create!(
   name: 'Paul Ferroggiaro',
   url: 'paul.jpg',
   alt: 'Picture of Paul Ferroggiaro',
+  width: 720,
+  height: 720,
   width_orig: 720,
   height_orig: 720,
 )
 
-Image.create!(
+ilinda = Image.create!(
   name: 'Linda Lacampagne',
   url: 'linda.jpg',
   alt: 'Picture of Linda Lacampagne',
+  width: 960,
+  height: 960,
   width_orig: 960,
   height_orig: 960,
 )
 
-Image.create!(
+inancy = Image.create!(
   name: 'Nancy Segreto',
   url: 'nancy.jpg',
   alt: 'Picture of Nancy Segreto',
+  width: 180,
+  height: 177,
   width_orig: 180,
   height_orig: 177,
 )
 
-Image.create!(
+ijoe = Image.create!(
   name: 'Joe Segreto',
   url: 'joe.jpg',
   alt: 'Picture of Joe Segreto',
+  width: 316,
+  height: 316,
   width_orig: 316,
   height_orig: 316,
 )
 
-Image.create!(
+iali = Image.create!(
   name: 'Ali Segreto',
   url: 'ali.jpg',
   alt: 'Picture of Ali Segreto',
+  width: 334,
+  height: 508,
   width_orig: 334,
   height_orig: 508,
 )
 
-p "Created #{Image.count} images"
 
 
 Cook.destroy_all
 
-images = Image.last(10).reverse
-10.times do |i|
-  Cook.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    date_of_birth: Faker::Date.between(80.years.ago, Date.today),
-    location: Faker::Address.city,
-    image: images[i]
-  )
-end
+linda = Cook.create(
+  first_name: "Linda",
+  last_name: "Lacampagne",
+  email: "linda@aol.com",
+  date_of_birth: Faker::Date.between(80.years.ago, Date.today),
+  location: 'Milbrae',
+  image: ilinda
+)
 
-p "Created #{Cook.count} cooks"
+paull = Cook.create(
+  first_name: "Paul",
+  last_name: "Lacampagne",
+  email: "linda@aol.com",
+  date_of_birth: Faker::Date.between(80.years.ago, Date.today),
+  location: 'Milbrae',
+  image: ilinda
+)
+
+joe = Cook.create(
+  first_name: "Joe",
+  last_name: "Segreto",
+  email: "",
+  date_of_birth: Faker::Date.between(80.years.ago, Date.today),
+  location: 'Greenbrae',
+  image: ijoe
+)
+
+paul = Cook.create(
+  first_name: "Paul",
+  last_name: "Ferroggiaro",
+  email: "",
+  date_of_birth: Faker::Date.between(80.years.ago, Date.today),
+  location: 'Arroyo Grande',
+  image: ipaul
+)
+
+anita = Cook.create(
+  first_name: "Anita",
+  last_name: "Ferroggiaro",
+  email: "",
+  date_of_birth: Faker::Date.between(80.years.ago, Date.today),
+  location: 'Daly City',
+  image: ianita
+)
+
+nancy = Cook.create(
+  first_name: "Nancy",
+  last_name: "Segreto",
+  email: "",
+  date_of_birth: Faker::Date.between(80.years.ago, Date.today),
+  location: 'Greenbrae',
+  image: inancy
+)
+
+ali = Cook.create(
+  first_name: "Ali",
+  last_name: "Segreto",
+  email: "",
+  date_of_birth: Faker::Date.between(80.years.ago, Date.today),
+  location: 'San Francisco',
+  image: iali
+)
+
 
 Category.destroy_all
 
-Category.create(
+antipasti = Category.create(
   name: 'antipasti',
   display_name: 'Antipasti',
-  order: 1
+  order: 1,
+  icon_name: 'antipasti'
 )
 
-Category.create(
+primi = Category.create(
   name: 'primi',
   display_name: 'Primi',
+  icon_name: 'primi',
   order: 2
 )
 
-Category.create(
+secondi = Category.create(
   name: 'secondi',
   display_name: 'Secondi',
+  icon_name: 'secondi',
   order: 3
 )
 
-Category.create(
+contorni = Category.create(
   name: 'contorni',
   display_name: 'Contorni',
+  icon_name: 'contorni',
   order: 4
 )
 
-Category.create(
+dolci = Category.create(
   name: 'dolci',
   display_name: 'Dolci',
+  icon_name: 'dolci',
   order: 5
 )
 
-p "Created #{Category.count} categories"
 
 Recipe.destroy_all
 
-10.times do |i|
-  Recipe.create(
-    title: Faker::Name,
-    difficulty: Faker::Number.between(1, 800),
-    story: Faker::Lorem.sentence(1),
-    ingredients: Faker::Food.ingredient,
-    description: Faker::Lorem.sentence(3),
-    category: Category.order('RANDOM()').first,
-    image: Image.order('RANDOM()').first,
-  )
-end
+salt = Ingredient.create(
+  name: 'salt'
+)
+grated_cheese = Ingredient.create(
+  name: 'grated cheese'
+)
+farina = Ingredient.create(
+  name: 'farina de castgne'
+)
+flour = Ingredient.create(
+  name: 'flour'
+)
+egg = Ingredient.create(
+  name: 'egg'
+)
 
+egg_yolk = Ingredient.create(
+  name: 'egg yolk'
+)
+butter = Ingredient.create(
+  name: 'butter'
+)
+potato = Ingredient.create(
+  name: 'potato',
+  extra: 'old potatoes work best'
+)
+ignocchi = Image.create(
+  name: 'Gnocchi',
+  url: ' https://s3-us-west-1.amazonaws.com/family-recipes/gnocchi-main.jpg',
+  alt: 'Gnocchi de castgne with pesto',
+  width: 1184,
+  height: 888,
+  width_orig: 1184,
+  height_orig: 888,
+)
+rgnocchi = Recipe.create(
+  title: 'Gnocchi de Castagne',
+  difficulty: 4,
+  memorandums: 'In honor of Mary Cirelli',
+  story: "This is our family’s all time favorite way to eat gnocchi. Both grandmotherswere experts at preparing these. As a child, I remember the bags of chestnutflour arriving from our cousins in Italy. These are always served with pestosauce. To splurge, I add cream cheese to the pesto and use freshly grated Parmesan cheese!",
+  description: "<p>Cook and mash potatoes. Put them through a wire strainer. Add butter, setaside and cool. Add egg and egg yolk, flour, grated cheese and salt. Kneaduntil smooth and manageable, adding a little more flour if needed.</p><p>Roll dough into long, ropelike strips and cut into 3/4 in pieces. Use the tines of a fork (backside) to roll the gnocchi and make the indentations. The gnocchi should resemble a shell. Store on a floured board until ready to cook.</p><p>Boil in rapidly boiling salted water. When gnocchi come to the top. Removethem from the pot. Drain and serve with your favorite sauce and grated cheese. *** Prepare instant mashed potatoes to make 6-8 servings according to the package instructions. Use no other seasonings.</p><p>Use 3/4-cup flour and 3/4-cup castagne flour, 1 tsp. olive oil and 2 eggs, lightly beaten. When rolling the gnocchi, you will need extra flour – be sureto add white flour so that the castagne flavor is not too strong. Complete gnocchi as directed above.</p>",
+  cooks: [linda],
+  category: primi,
+  image: ignocchi,
+)
+IngredientAmount.create(
+  amount: '1/4',
+  unit: 'tsp',
+  ingredient: salt,
+  recipe: rgnocchi
+)
+IngredientAmount.create(
+  amount: '1/4',
+  unit: 'cup',
+  ingredient: grated_cheese,
+  recipe: rgnocchi
+)
+IngredientAmount.create(
+  amount: '1',
+  unit: 'cup',
+  ingredient: farina,
+  recipe: rgnocchi
+)
+IngredientAmount.create(
+  amount: '1',
+  unit: 'cup',
+  ingredient: flour,
+  recipe: rgnocchi
+)
+IngredientAmount.create(
+  amount: '1',
+  unit: '',
+  ingredient: egg_yolk,
+  recipe: rgnocchi
+)
+IngredientAmount.create(
+  amount: '1',
+  unit: '',
+  ingredient: egg,
+  recipe: rgnocchi
+)
+IngredientAmount.create(
+  amount: '1/4',
+  unit: 'cube',
+  ingredient: butter,
+  recipe: rgnocchi
+)
+IngredientAmount.create(
+  amount: '2',
+  unit: 'lb',
+  ingredient: potato,
+  recipe: rgnocchi
+)
+
+p "Created #{Category.count} categories"
+p "Created #{Image.count} images"
+p "Created #{Cook.count} cooks"
 p "Created #{Recipe.count} recipes"

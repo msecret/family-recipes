f47a37dd606d2b0889d2052e493a7a678b8a263c
Marco Segreto <msegreto@ideo.com>
2018-09-14 09:48:52 -0700

    Optional image on recipe

--- api/app/models/recipe.rb
+++ api/app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
 
-  belongs_to :image
+  belongs_to :image, optional: true
   belongs_to :category
   has_and_belongs_to_many :cooks
   has_many :ingredient_amounts
